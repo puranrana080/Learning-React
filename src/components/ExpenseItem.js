f27f63e3519687669459aa0d07d7b49e5b1c0427
Puran Singh Rana <puransinghrana080@gmail.com>
2025-04-06 10:00:17 +0530

    Styling components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,12 @@
+import './ExpenseItem.css'
+
 function ExpenseItem(){
-    return <div>
+    return <div className='expense-item'>
         <div>06 April 2025</div>
-        <div>
+        <div className='expense-item__location'>Delhi</div>
+        <div className='expense-item__description'>
             <h2>Book</h2>
-            <div>$20</div>
+            <div className='expense-item__price'>$20</div>
         </div>
     </div>
 }
