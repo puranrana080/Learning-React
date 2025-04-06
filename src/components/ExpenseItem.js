8735dd643e51ac2e60f8c04d174215640a950909
Puran Singh Rana <puransinghrana080@gmail.com>
2025-04-06 17:49:11 +0530

    formatting date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,18 @@
 import './ExpenseItem.css'
+const months = ['January','Febuary','March','April','May','June','July','August','September','October','November','December']
+
 
 function ExpenseItem(props){
+    const month=months[props.date.getMonth()]
+    const year=props.date.getFullYear()
+    const day=props.date.getDate()
 
     return <div className='expense-item'>
-        <div>{props.date.toISOString()}</div>
+        <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+        </div>
         <div className='expense-item__location'>{props.location}</div>
         <div className='expense-item__description'>
             <h2>{props.title}</h2>
