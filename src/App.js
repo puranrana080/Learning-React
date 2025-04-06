4b9bddcee243deb6d010ebb60714e707581f4064
Puran Singh Rana <puransinghrana080@gmail.com>
2025-04-06 09:47:13 +0530

    writing first jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import ExpenseItem from './components/ExpenseItem'
+
+
 
 function App() {
   return (
     <div>
       <h1>Lets get started</h1>
       <p>I am Learning React!</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,10 @@
+function ExpenseItem(){
+    return <div>
+        <div>06 April 2025</div>
+        <div>
+            <h2>Book</h2>
+            <div>$20</div>
+        </div>
+    </div>
+}
+export default ExpenseItem;
