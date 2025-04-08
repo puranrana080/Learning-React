c74fc1d45cc3649729ce16ab5ac966fa031f49a3
Puran Singh Rana <puransinghrana080@gmail.com>
2025-04-09 01:15:33 +0530

    Conditional Rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,30 +1,44 @@
-import React,{useState} from "react"
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
-import ExpensesFilter from "./ExpensesFilter"
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
-import Card from "../UI/Card"
+import Card from "../UI/Card";
 
 function Expenses(props) {
-  const [filteredYear,setFilteredYear]=useState("2023")
+  const [filteredYear, setFilteredYear] = useState("2023");
 
-const changeFilterHandler=(selectedYear)=>{
-  setFilteredYear(selectedYear)
-}
-const filteredExpenses=props.expenses.filter((item)=>(item.date.getFullYear()==filteredYear))
+  const changeFilterHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  const filteredExpenses = props.expenses.filter(
+    (item) => item.date.getFullYear() == filteredYear
+  );
+
+  let expenseContent = <p>No Expense Found</p>;
+  if (filteredExpenses.length > 0) {
+    expenseContent = filteredExpenses.map((expense) => {
+      return (
+        <ExpenseItem
+          key={expense.id}
+          date={expense.date}
+          title={expense.title}
+          price={expense.price}
+        ></ExpenseItem>
+      );
+    });
+  }
 
   return (
     <Card className="expenses">
-       <ExpensesFilter selected={filteredYear} onChangeFilter={changeFilterHandler}></ExpensesFilter>
-      {filteredExpenses.map((expense) => {
-        return (
-          <ExpenseItem
-            key={expense.id}
-            date={expense.date}
-            title={expense.title}
-            price={expense.price}
-          ></ExpenseItem>
-        );
-      })}
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={changeFilterHandler}
+      ></ExpensesFilter>
+
+      {expenseContent}
+      {filteredExpenses.length === 1 && (
+        <p>Only one expense found. Please add more</p>
+      )}
     </Card>
   );
 }
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,23 +1,27 @@
-import React from "react"
-import "./ExpensesFilter.css"
+import React from "react";
+import "./ExpensesFilter.css";
 
-const ExpensesFilter=(props)=>{
+const ExpensesFilter = (props) => {
+  const dropDownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
+  };
 
-    const dropDownChangeHandler=(event)=>{
-        props.onChangeFilter(event.target.value)
-    }
-
-    return (
-        <div className="expenses-filter">
-            <div className="expenses-filter__control">
-                <label htmlFor="year-dropdown">Filetr By Year</label>
-                <select value={props.selected} onChange={dropDownChangeHandler} id="year-dropdown">
-                    <option value="2023">2023</option>
-                    <option value="2024">2024</option>
-                    <option value="2025">2025</option>
-                </select>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label htmlFor="year-dropdown">Filetr By Year</label>
+        <select
+          value={props.selected}
+          onChange={dropDownChangeHandler}
+          id="year-dropdown"
+        >
+          <option value="2023">2023</option>
+          <option value="2024">2024</option>
+          <option value="2025">2025</option>
+          <option value="2026">2026</option>
+        </select>
+      </div>
+    </div>
+  );
+};
 export default ExpensesFilter;
